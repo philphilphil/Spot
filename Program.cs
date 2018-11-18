d4d5eb8e1523df76b5fc50ec7013118b57508cb8
Phil Baum <phil@phib.io>
2018-11-18 23:41:00 +0100

    Cleanup and reorganize

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using CHEP.Helpers;
 using ChessDotNet;
 
 namespace CHEP
@@ -9,66 +10,14 @@ namespace CHEP
     {
         static void Main(string[] args)
         {
-            //PlayInCLI();
-
             UCI uci = new UCI();
             uci.Start();
+
+            //used to play in console
+            //PlayInConsole local = new PlayInConsole();
+            //local.Play();
         }
 
-        private static void PlayInCLI()
-        {
-            // Let's start by creating a chess game instance.
-            //min max test fen 8/8/2R2B2/2N5/3b4/8/8/8 w KQkq -
 
-            ChessGame game = new ChessGame(@"7k/8/8/2R2B2/2N5/3b4/8/7K w - - 0 1");
-            Engine engine = new Engine();
-
-            while (true)
-            {
-                var nextMove = UpdateConsoleAndGetMove(game);
-
-                if (game.WhoseTurn == Player.Black)
-                {
-                    //Random move
-                    //IEnumerable<Move> validMoves = game.GetValidMoves(Player.Black);
-                    //Move randomMove = validMoves.OrderBy(t => Guid.NewGuid()).FirstOrDefault();
-                    //MoveType type = game.ApplyMove(randomMove, true);
-
-                    //calculate move
-                    Move bestMove = engine.CalculateBestMove(game);
-                    MoveType type = game.ApplyMove(bestMove, true);
-                }
-                else
-                {
-                    String[] move = nextMove.Split(" ");
-                    Move playerMove = new Move(move[0], move[1], Player.White);
-                    bool isValid = game.IsValidMove(playerMove);
-
-                    if (!isValid)
-                    {
-                        continue;
-                    }
-                    MoveType type = game.ApplyMove(playerMove, true);
-
-                }
-            }
-            Console.ReadKey();
-        }
-
-        private static string UpdateConsoleAndGetMove(ChessGame game)
-        {
-            Console.Clear();
-            CLITools.DrawBoard(game);
-            Console.WriteLine("It's this color's turn: {0}", game.WhoseTurn);
-
-            if (game.WhoseTurn == Player.White)
-            {
-                Console.Write("Enter move: ");
-                return Console.ReadLine();
-
-            }
-
-            return null;
-        }
     }
 }
