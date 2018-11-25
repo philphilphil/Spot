b4d53aa2fc4847e07d075e60c04e552292e607ed
Phil Baum <phil@phib.io>
2018-11-25 14:05:33 +0100

    Added piece position evals and some better documentation

--- Engine/Engine.cs
+++ Engine/Engine.cs
@@ -9,29 +9,35 @@ namespace CHEP
 {
     class Engine
     {
-        DateTime startTime;
-        int[,] pawnWhite;
-        int[,] pawnBlack;
+        DateTime StartTime;
+        int[,] PawnWhite, KnightWhite, BishopWhite, RookWhite, QueenWhite, KingWhite;
+        int[,] PawnBlack, KnightBlack, BishopBlack, RookBlack, QueenBlack, KingBlack;
         int nodeCounter = 0;
 
+
+        public Engine()
+        {
+            SetupPieceSquareEvals();
+        }
+
+        /// <summary>
+        /// Calculation starting point. itterate through all legal moves and start the minimax calculation 
+        /// </summary>
+        /// <param name="game"></param>
+        /// <returns></returns>
         public Move CalculateBestMove(ChessGame game)
         {
             Move bestMove = null;
             int bestRating = -9999;
+            this.StartTime = DateTime.Now;
             String originalPositionFen = game.GetFen();
 
-            SetupPieceSquareEvals();
-
-            //get all possible legal moves
             List<Move> validMoves = game.GetValidMoves(game.WhoseTurn).ToList();
 
-            this.startTime = DateTime.Now;
-
-            //itterate through all possible moves
             foreach (var move in validMoves)
             {
-                //dummy values for now
-                Console.WriteLine("info depth 1 score cp 1 time " + MillisecondsSinceStart(this.startTime) + " nodes " + nodeCounter.ToString());
+                //UCI Info output
+                Console.WriteLine("info depth 1 score cp 1 time " + MillisecondsSinceStart(this.StartTime) + " nodes " + nodeCounter.ToString());
 
                 //set fen back to original position
                 game = new ChessGame(originalPositionFen);
@@ -44,7 +50,7 @@ namespace CHEP
                 bool maximisingPlayer = turnMadeBy == Player.White ? true : false;
                 var boardRating = MiniMaxBestMove(game, 4, -9999, 9999, maximisingPlayer);
 
-                //if its blacks turn, take negative of the rating because blacks pieces are valued in -
+                //if its blacks turn, take negative of the rating because blacks wants to minimize
                 if (turnMadeBy == Player.Black)
                 {
                     boardRating = -boardRating; //maybe: -Math.Abs(boardRating);
@@ -61,43 +67,15 @@ namespace CHEP
             return bestMove;
         }
 
-        private void SetupPieceSquareEvals()
-        {
-            // Piece-Square Tables https://www.chessprogramming.org/Simplified_Evaluation_Function
-            // pawn
-            this.pawnWhite = new int[,]
-            {
-                { 0,  0,  0,  0,  0,  0,  0,  0 },
-                { 50, 50, 50, 50, 50, 50, 50, 50},
-                { 10, 10, 20, 30, 30, 20, 10, 10},
-                { 5,  5, 10, 25, 25, 10,  5,  5},
-                { 0,  0,  0, 20, 20,  0,  0,  0},
-                { 5, -5,-10,  0,  0,-10, -5,  5},
-                { 5, 10, 10,-20,-20, 10, 10,  5},
-                { 0,  0,  0,  0,  0,  0,  0,  0 }
-            };
-
-            this.pawnBlack = new int[,]
-            {
-                { 0,  0,  0,  0,  0,  0,  0,  0 },
-                { 5, 10, 10,-20,-20, 10, 10,  5},
-                { 5, -5,-10,  0,  0,-10, -5,  5},
-                { 0,  0,  0, 20, 20,  0,  0,  0},
-                { 5,  5, 10, 25, 25, 10,  5,  5},
-                { 10, 10, 20, 30, 30, 20, 10, 10},
-                { 50, 50, 50, 50, 50, 50, 50, 50},
-                { 0,  0,  0,  0,  0,  0,  0,  0 }
-            };
-        }
-
-        private string MillisecondsSinceStart(DateTime startDate)
-        {
-            TimeSpan span = DateTime.Now - startDate;
-            int ms = (int)span.TotalMilliseconds;
-            return ms.ToString();
-        }
-
-        //MiniMax - https://en.wikipedia.org/wiki/Minimax#Pseudocode
+        /// <summary>
+        /// MiniMax - https://en.wikipedia.org/wiki/Minimax#Pseudocode
+        /// </summary>
+        /// <param name="game"></param>
+        /// <param name="depth"></param>
+        /// <param name="alpha"></param>
+        /// <param name="beta"></param>
+        /// <param name="maximizingPlayer"></param>
+        /// <returns></returns>
         public int MiniMaxBestMove(ChessGame game, int depth, int alpha, int beta, bool maximizingPlayer)
         {
             nodeCounter++;
@@ -193,6 +171,18 @@ namespace CHEP
         }
 
         /// <summary>
+        /// Calculates ms between the startDate and now
+        /// </summary>
+        /// <param name="startDate"></param>
+        /// <returns></returns>
+        private string MillisecondsSinceStart(DateTime startDate)
+        {
+            TimeSpan span = DateTime.Now - startDate;
+            int ms = (int)span.TotalMilliseconds;
+            return ms.ToString();
+        }
+
+        /// <summary>
         /// Get a value for each piece for each player
         /// </summary>
         /// <param name="piece"></param>
@@ -204,28 +194,179 @@ namespace CHEP
             switch (piece.GetFenCharacter().ToString().ToLower())
             {
                 case "p":
-                    value = 100 + (piece.Owner == Player.White ? this.pawnWhite[row - 1, column - 1] : this.pawnBlack[row - 1, column - 1]);
+                    value = 100 + (piece.Owner == Player.White ? this.PawnWhite[row - 1, column - 1] : this.PawnBlack[row - 1, column - 1]);
                     break;
                 case "n":
-                    value = 320;
+                    value = 320 + (piece.Owner == Player.White ? this.KnightWhite[row - 1, column - 1] : this.KnightBlack[row - 1, column - 1]);
                     break;
                 case "b":
-                    value = 330;
+                    value = 330 + (piece.Owner == Player.White ? this.BishopWhite[row - 1, column - 1] : this.BishopBlack[row - 1, column - 1]);
                     break;
                 case "r":
-                    value = 500;
+                    value = 500 + (piece.Owner == Player.White ? this.RookWhite[row - 1, column - 1] : this.RookBlack[row - 1, column - 1]);
                     break;
                 case "q":
-                    value = 900;
+                    value = 900 + (piece.Owner == Player.White ? this.QueenWhite[row - 1, column - 1] : this.QueenBlack[row - 1, column - 1]);
                     break;
                 case "k":
-                    value = 2000;
+                    value = 2000 + (piece.Owner == Player.White ? this.KingWhite[row - 1, column - 1] : this.KingBlack[row - 1, column - 1]);
                     break;
                 default:
                     break;
             }
 
             return value;
+        }
+
+        /// <summary>
+        /// Piece-Square Tables https://www.chessprogramming.org/Simplified_Evaluation_Function
+        /// </summary>
+        private void SetupPieceSquareEvals()
+        {
+            this.PawnWhite = new int[,]
+            {
+                { 0,  0,  0,  0,  0,  0,  0,  0},
+                { 50, 50, 50, 50, 50, 50, 50, 50},
+                { 10, 10, 20, 30, 30, 20, 10, 10},
+                { 5,  5, 10, 25, 25, 10,  5,  5},
+                { 0,  0,  0, 20, 20,  0,  0,  0},
+                { 5, -5,-10,  0,  0,-10, -5,  5},
+                { 5, 10, 10,-20,-20, 10, 10,  5},
+                { 0,  0,  0,  0,  0,  0,  0,  0 }
+            };
+
+            this.PawnBlack = new int[,]
+            {
+                { 0,  0,  0,  0,  0,  0,  0,  0},
+                { 5, 10, 10,-20,-20, 10, 10,  5},
+                { 5, -5,-10,  0,  0,-10, -5,  5},
+                { 0,  0,  0, 20, 20,  0,  0,  0},
+                { 5,  5, 10, 25, 25, 10,  5,  5},
+                { 10, 10, 20, 30, 30, 20, 10, 10},
+                { 50, 50, 50, 50, 50, 50, 50, 50},
+                { 0,  0,  0,  0,  0,  0,  0,  0 }
+            };
+
+            this.KnightWhite = new int[,]
+            {
+                {-50,-40,-30,-30,-30,-30,-40,-50},
+                {-40,-20,  0,  0,  0,  0,-20,-40},
+                {-30,  0, 10, 15, 15, 10,  0,-30},
+                {-30,  5, 15, 20, 20, 15,  5,-30},
+                {-30,  0, 15, 20, 20, 15,  0,-30},
+                {-30,  5, 10, 15, 15, 10,  5,-30},
+                {-40,-20,  0,  5,  5,  0,-20,-40},
+                {-50,-40,-30,-30,-30,-30,-40,-50}
+            };
+
+            this.KnightBlack = new int[,]
+            {
+                {-50,-40,-30,-30,-30,-30,-40,-50},
+                {-40,-20,  0,  5,  5,  0,-20,-40},
+                {-30,  5, 10, 15, 15, 10,  5,-30},
+                {-30,  0, 15, 20, 20, 15,  0,-30},
+                {-30,  5, 15, 20, 20, 15,  5,-30},
+                {-30,  0, 10, 15, 15, 10,  0,-30},
+                {-40,-20,  0,  0,  0,  0,-20,-40},
+                {-50,-40,-30,-30,-30,-30,-40,-50}
+            };
+
+            this.BishopWhite = new int[,]
+            {
+                {-20,-10,-10,-10,-10,-10,-10,-20},
+                {-10,  0,  0,  0,  0,  0,  0,-10},
+                {-10,  0,  5, 10, 10,  5,  0,-10},
+                {-10,  5,  5, 10, 10,  5,  5,-10},
+                {-10,  0, 10, 10, 10, 10,  0,-10},
+                {-10, 10, 10, 10, 10, 10, 10,-10},
+                {-10,  5,  0,  0,  0,  0,  5,-10},
+                {-20,-10,-10,-10,-10,-10,-10,-20}
+            };
+
+            this.BishopBlack = new int[,]
+            {
+                {-20,-10,-10,-10,-10,-10,-10,-20},
+                {-10,  5,  0,  0,  0,  0,  5,-10},
+                {-10, 10, 10, 10, 10, 10, 10,-10},
+                {-10,  0, 10, 10, 10, 10,  0,-10},
+                {-10,  5,  5, 10, 10,  5,  5,-10},
+                {-10,  0,  5, 10, 10,  5,  0,-10},
+                {-10,  0,  0,  0,  0,  0,  0,-10},
+                {-20,-10,-10,-10,-10,-10,-10,-20}
+            };
+
+            this.RookWhite = new int[,]
+            {
+                {  0,  0,  0,  0,  0,  0,  0,  0},
+                {  5, 10, 10, 10, 10, 10, 10,  5},
+                { -5,  0,  0,  0,  0,  0,  0, -5},
+                { -5,  0,  0,  0,  0,  0,  0, -5},
+                { -5,  0,  0,  0,  0,  0,  0, -5},
+                { -5,  0,  0,  0,  0,  0,  0, -5},
+                { -5,  0,  0,  0,  0,  0,  0, -5},
+                {  0,  0,  0,  5,  5,  0,  0,  0}
+            };
+
+            this.RookBlack = new int[,]
+            {
+                {  0,  0,  0,  5,  5,  0,  0,  0},
+                { -5,  0,  0,  0,  0,  0,  0, -5},
+                { -5,  0,  0,  0,  0,  0,  0, -5},
+                { -5,  0,  0,  0,  0,  0,  0, -5},
+                { -5,  0,  0,  0,  0,  0,  0, -5},
+                { -5,  0,  0,  0,  0,  0,  0, -5},
+                {  5, 10, 10, 10, 10, 10, 10,  5},
+                {  0,  0,  0,  0,  0,  0,  0,  0}
+            };
+
+            this.QueenWhite = new int[,]
+            {
+                {-20,-10,-10, -5, -5,-10,-10,-20},
+                {-10,  0,  0,  0,  0,  0,  0,-10},
+                {-10,  0,  5,  5,  5,  5,  0,-10},
+                { -5,  0,  5,  5,  5,  5,  0, -5},
+                {  0,  0,  5,  5,  5,  5,  0, -5},
+                {-10,  5,  5,  5,  5,  5,  0,-10},
+                {-10,  0,  5,  0,  0,  0,  0,-10},
+                {-20,-10,-10, -5, -5,-10,-10,-20}
+            };
+
+            this.QueenBlack = new int[,]
+             {
+                {-20,-10,-10, -5, -5,-10,-10,-20},
+                {-10,  0,  5,  0,  0,  0,  0,-10},
+                {-10,  5,  5,  5,  5,  5,  0,-10},
+                {  0,  0,  5,  5,  5,  5,  0, -5},
+                { -5,  0,  5,  5,  5,  5,  0, -5},
+                {-10,  0,  5,  5,  5,  5,  0,-10},
+                {-10,  0,  0,  0,  0,  0,  0,-10},
+                {-20,-10,-10, -5, -5,-10,-10,-20}
+            };
+
+            this.KingWhite = new int[,]
+            {
+                {-30,-40,-40,-50,-50,-40,-40,-30},
+                {-30,-40,-40,-50,-50,-40,-40,-30},
+                {-30,-40,-40,-50,-50,-40,-40,-30},
+                {-30,-40,-40,-50,-50,-40,-40,-30},
+                {-20,-30,-30,-40,-40,-30,-30,-20},
+                {-10,-20,-20,-20,-20,-20,-20,-10},
+                { 20, 20,  0,  0,  0,  0, 20, 20},
+                { 20, 30, 10,  0,  0, 10, 30, 20}
+            };
+
+            this.KingBlack = new int[,]
+            {
+                { 20, 30, 10,  0,  0, 10, 30, 20},
+                { 20, 20,  0,  0,  0,  0, 20, 20},
+                {-10,-20,-20,-20,-20,-20,-20,-10},
+                {-20,-30,-30,-40,-40,-30,-30,-20},
+                {-30,-40,-40,-50,-50,-40,-40,-30},
+                {-30,-40,-40,-50,-50,-40,-40,-30},
+                {-30,-40,-40,-50,-50,-40,-40,-30},
+                {-30,-40,-40,-50,-50,-40,-40,-30}
+            };
+
         }
     }
 }
