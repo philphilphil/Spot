c30d6d249dd3fbefdbca0177fa0b212895866a93
Phil Baum <phil@phib.io>
2020-12-28 21:35:08 +0100

    renamed builds folder to bin

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 version=1.0
 time=$(date +"%j%H%M")
-go build -o builds/Spot_$version\_darwin -ldflags="-X 'main.BuildTime=$time' -X 'main.BuildVersion=$version'" . 
-env GOOS=linux GOARCH=arm go build -o builds/Spot_$version\_arm -ldflags="-X 'main.BuildTime=$time' -X 'main.BuildVersion=$version'" . 
-env GOOS=windows GOARCH=amd64 go build -o builds/Spot_$version\_win.exe -ldflags="-X 'main.BuildTime=$time' -X 'main.BuildVersion=$version'" . 
+go build -o bin/Spot_$version\_darwin -ldflags="-X 'main.BuildTime=$time' -X 'main.BuildVersion=$version'" . 
+env GOOS=linux GOARCH=arm go build -o bin/Spot_$version\_arm -ldflags="-X 'main.BuildTime=$time' -X 'main.BuildVersion=$version'" . 
+env GOOS=windows GOARCH=amd64 go build -o bin/Spot_$version\_win.exe -ldflags="-X 'main.BuildTime=$time' -X 'main.BuildVersion=$version'" . 
