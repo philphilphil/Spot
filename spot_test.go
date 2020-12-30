f1bb4188cf0cfb8bdbb04b92c831fce05e1c433c
Phil Baum <phil@phib.io>
2020-12-30 13:25:03 +0100

    Added tests for debugging matescore

--- spot_test.go
+++ spot_test.go
@@ -9,7 +9,7 @@ import (
 
 //TODO: refactor all calc test functions into one and use engine test epd
 func TestCalculationBlack_1(t *testing.T) {
-	//debug =  true
+	debug = true
 	testGame := getGameFromFen(strings.Fields("fen rnbqkbnr/5ppp/4p3/2PN2B1/1P2P3/p4N2/P1P1BPPP/1R1QK2R b Kkq - 0 1"))
 	bestMove := calculateBestMove(testGame)
 
@@ -52,7 +52,8 @@ func TestCalculationMateInOnes(t *testing.T) {
 	mateInOneFens["a1a8"] = "7k/4pppp/8/8/6P1/8/5N2/R2Q1K2 w Q - 0 1"
 	mateInOneFens["f1f7"] = "4rk2/4pppp/8/6N1/6P1/8/8/R4Q1K w Q - 0 1"
 	mateInOneFens["d5b3"] = "8/8/8/3Q4/7P/k1K4P/8/8 w - - 15 74"
-	mateInOneFens["h1b7"] = "6k1/K7/8/8/8/8/1r6/3n3q b - - 3 65"
+	mateInOneFens["h1b7"] = "6k1/K7/8/8/8/8/1r6/3n3q b - - 3 65" //black
+	mateInOneFens["h1b7"] = "6K1/k7/8/8/8/8/1R6/3N3Q w - - 0 1"  // white
 
 	CheckFenForBestMove(mateInOneFens, t)
 }
@@ -68,13 +69,31 @@ func TestCalculationMateInTwos(t *testing.T) {
 	CheckFenForBestMove(mateInTwoFens, t)
 }
 
+func TestCalculationCheckMateDebugger(t *testing.T) {
+	debug = true
+	var mates = make(map[string]string)
+	// mates["h1b7"] = "6k1/K7/8/8/8/8/1r6/3n3q b - - 3 65"                       // b M1
+	// mates["h1b7"] = "6K1/k7/8/8/8/8/1R6/3N3Q w - - 0 1"                        // w M1
+	// mates["f1f7"] = "4rk2/4pppp/8/6N1/6P1/8/8/R4Q1K w Q - 0 1"                 // w M1
+	// mates["d5b3"] = "8/8/8/3Q4/7P/k1K4P/8/8 w - - 15 74"                       // w M1
+	// mates["g2g1"] = "2kr4/1bp5/ppqp1N1p/4pp1Q/2Pn4/BP1P4/P4RrP/R4K2 b - - 0 1" // b M2
+	//mates["a6h6"] = "5Knk/7b/R7/8/7B/8/8/8 w - - 0 1"               // w M2
+	//mates["d1f3"] = "8/8/4pbk1/N1P5/2P1K2p/Q2R4/5q1P/3b4 b - - 0 1" // b M2
+	mates["d4h4"] = "7k/2p1Q1pp/P7/P1p3R1/3q4/5PK1/8/7r b - - 0 1" // b M2
+	//mates["d2c3"] = "8/8/8/3Q4/k6P/7P/3K4/8 w - - 13 73"                       // w M2
+	// mates["h7h6"] = "2r4k/5ppp/8/8/8/4Q3/PPPPPPPP/RNB1KBNR b KQ - 0 1"         //M1 for w oponnent if b does wrong move
+	// mates["a1g7"] = "1k6/5ppp/8/1q6/6b1/6N1/8/Q3K3 w - - 0 1"                  //M1 for blback oponnent if w does wrong move
+
+	CheckFenForBestMove(mates, t)
+}
+
 func CheckFenForBestMove(movePos map[string]string, t *testing.T) {
 	for move, fen := range movePos {
 		board := dragontoothmg.ParseFen(fen)
 		bestMove := calculateBestMove(board)
 
 		if bestMove.String() != move {
-			t.Errorf("Move wrong got: %v, want: %v", bestMove.String(), move)
+			t.Errorf("Move wrong got: %v, want: %v, Fen: https://lichess.org/analysis/fromPosition/%v", bestMove.String(), move, strings.ReplaceAll(fen, " ", "_"))
 		}
 	}
 }
